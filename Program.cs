e967c9e61e6cc3c64bb8ec16d595c0af60dcea17
HarryLundblad <90604842+HarryLundblad@users.noreply.github.com>
2021-09-24 11:51:52 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace sak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ord = new string[5];
+            
+            Console.WriteLine("Skriv fem tal");
+
+            for (int i = 0; i < ord.Length; i++)
+            {
+                tal[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Här är de sorterade alfabetiskt.");
+
+            Array.Sort(ord);
+
+            for (int i = 0; i < ord.Length; i++)
+            {
+                Console.WriteLine(ord[i]);
+            }
+
+
+
+
+        }
+    }
+}
